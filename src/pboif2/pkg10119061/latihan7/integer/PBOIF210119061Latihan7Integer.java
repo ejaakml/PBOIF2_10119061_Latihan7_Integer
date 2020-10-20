6858c24294394d47cc5b2a8925c6ed9686c74535
ejaakml <eximindotong@yahoo.com>
2020-10-20 17:45:31 +0700

    d

---
+++ src/pboif2/pkg10119061/latihan7/integer/PBOIF210119061Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan7.integer;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class PBOIF210119061Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       byte b;
+       short s;
+       int i;
+       long l;
+       b = 120;
+       s = 32767;
+       i = 1_000_000_000;
+       l = 90000000000L;
+       System.out.println("byte : " + b);
+       System.out.println("short : " + s);
+       System.out.println("int : " + i);
+       System.out.println("long : " + l);
+       
+       
+       
+        
+    }
+    
+}
